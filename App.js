47f13494639cf9d9c245ca75a1a5f835dd696b16
Jeremias Marinho <jeremias10@msn.com>
2021-09-28 20:27:12 -0300

    Criação de primeiros componentes

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './componets/Caritem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <View style={styles.carContainer}>
-          <ImageBackground source={require('./assets/images/ModelX.jpeg')} 
-          style={styles.image}/>
-          <View style={styles.titles}>
-              <Text style={styles.title}>Model S</Text>
-              <Text style={styles.subtitle}>Starting at $69,420</Text>
-          </View>
+       <CarItem />
 
-        </View>
-
-      <StatusBar style="auto" />
+       <StatusBar style="auto" />
     </View>
   );
 }
