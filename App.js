da27f484f08869cbcd014368af7d9b468bf5a455
Jeremias Marinho <jeremias10@msn.com>
2021-09-28 19:41:21 -0300

    Inserindo a primeira tela

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.carContainer}>
+          <ImageBackground source={require('./assets/images/ModelX.jpeg')} 
+          style={styles.image}/>
+          <View style={styles.titles}>
+              <Text style={styles.title}>Model S</Text>
+              <Text style={styles.subtitle}>Starting at $69,420</Text>
+          </View>
+
+        </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +27,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles:{
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center'
+  },
+  title:{
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle:{
+    fontSize: 16,
+    color: '#5c5e62'
+  },
+  image:{
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  }
 });
