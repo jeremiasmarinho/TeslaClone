d117b39d88e8e7f1bc4dccc9336e6168ccce9b81
Jeremias Marinho <jeremias10@msn.com>
2021-09-29 02:05:39 -0300

    Projeto finalizado

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, } from 'react-native';
 import CarsList from './componets/CarsList';
+import Header from './componets/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+        <Header/>
         <CarsList/>
 
        <StatusBar style="auto" />
--- componets/Caritem/index.js
+++ componets/Caritem/index.js
@@ -15,7 +15,7 @@ const CarItem = (props) => {
     <View style={styles.titles}>
         <Text style={styles.title}>{name}</Text>
         <Text style={styles.subtitle}>
-          {tagline}
+          {tagline}{' '}
           <Text style={styles.subtitleCTA}>
             {taglineCTA}
           </Text>
